2b3ace51e17226d3cebce84751d24a81a5016b97
Sezonov Danylo <prizrak7p@gmail.com>
2024-04-30 19:38:27 +0300

    MEs

--- Project/Tetris/Figure.py
+++ Project/Tetris/Figure.py
@@ -1,4 +1,25 @@
-from abc import ABCMeta, abstractmethod
+class Figure:
+    def __init__(self, a, n):
+        self.coordinats = a
+        self.color = n
+        self.active = 1
 
 
-class Figure(METAclass = ABCMeta):
+
+    def povorot(self):
+        a = self.coordinats
+        for i in range(0, 4):
+            x = self.coordinats[i][0]
+            y = self.coordinats[i][1]
+            self.coordinats[i][0], self.coordinats[i][1] = y, 5-x
+        return(Figure(self.coordinats, self.color))
+
+
+
+
+a = [[4,1], [4,2], [4,3], [3,2]]
+Fig = Figure(a, 5)
+print(Fig.povorot().coordinats)
+
+
+
---
+++ Project/Tetris/Figures.py
@@ -0,0 +1,9 @@
+from Figure import Figure
+
+
+
+class Figure1(Figure):
+    def __init__(self, n):
+        super().__init__(n)
+
+
